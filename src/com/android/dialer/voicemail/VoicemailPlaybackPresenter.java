a33f6ef116fb25aef5268373de4bcfb1fb52be40
masafumi miya <masafumi.x.miya@sonymobile.com>
2016-11-17 19:09:13 +0900

    Fixed the issue that speaker icon does not match valume
    Fixed a problem that the display of the icon and the value held
    internally will be misaligned when the screen is redrawn by interrupt.
    
    Change-Id: Ib0cf9b698f8090975e9e1d0a57f20be71d7e427f

--- src/com/android/dialer/voicemail/VoicemailPlaybackPresenter.java
+++ src/com/android/dialer/voicemail/VoicemailPlaybackPresenter.java
@@ -12,6 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
  */
 
 package com.android.dialer.voicemail;
@@ -266,6 +267,9 @@ public class VoicemailPlaybackPresenter implements MediaPlayer.OnPreparedListene
             // media player.
             mPosition = mMediaPlayer.getCurrentPosition();
             onPrepared(mMediaPlayer);
+            // set saved value.
+            setSpeakerphoneOn(mIsSpeakerphoneOn);
+            mVoicemailAudioManager.setSpeakerphoneOn(mIsSpeakerphoneOn);
         } else {
             if (!voicemailUri.equals(mVoicemailUri)) {
                 mVoicemailUri = voicemailUri;
